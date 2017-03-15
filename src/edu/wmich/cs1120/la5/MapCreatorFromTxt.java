13e41593a22091317c57e54e3aa4853b8d99d01e
Maxunm <cowpeople12321@gmail.com>
2017-03-15 12:21:53 -0400

    Added getScanner to MapCreatorFromTxt.

--- src/edu/wmich/cs1120/la5/MapCreatorFromTxt.java
+++ src/edu/wmich/cs1120/la5/MapCreatorFromTxt.java
@@ -1,5 +1,7 @@
 package edu.wmich.cs1120.la5;
 
+import jdk.nashorn.internal.codegen.MapCreator;
+
 import java.io.IOException;
 
 public class MapCreatorFromTxt implements IMapCreator {
@@ -8,6 +10,10 @@ public class MapCreatorFromTxt implements IMapCreator {
 	public void scanTerrain(String fileName, int threshold) throws IOException {
 		// TODO Auto-generated method stub
 
+	}
+
+	public TerrainScanner getScanner(){
+		return null;
 	}
 
 }
