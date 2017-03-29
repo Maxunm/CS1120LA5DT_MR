6df1a918784014d1efbb7752a468213cc8cf12a0
Maxunm <cowpeople12321@gmail.com>
2017-03-29 11:07:23 -0400

    Main set up

--- src/edu/wmich/cs1120/la5/LA5Main.java
+++ src/edu/wmich/cs1120/la5/LA5Main.java
@@ -1,9 +1,14 @@
 package edu.wmich.cs1120.la5;
 
+import edu.wmich.cs1120.la5.gui.MainWindow;
+
 /* Assignment: ...
  * Author: Merrick Rumel
  * Date: 3/15/2017
  * Reference: ...
  */
 public class LA5Main {
+    public static void main(String[] args){
+        MainWindow.startGUI();
+    }
 }
