fc51998c4dea97c5bdc022276d3919d61aa55628
Maxunm <cowpeople12321@gmail.com>
2017-03-29 23:26:25 -0400

    Final Code Push, Need Review.

--- src/edu/wmich/cs1120/la5/EnergyAnalyzer.java
+++ src/edu/wmich/cs1120/la5/EnergyAnalyzer.java
@@ -8,26 +8,33 @@ import java.util.ArrayList;
  * Reference: ...
  */
 public class EnergyAnalyzer implements IRover {
+    private ArrayList<IArea> path;
+    private String total;
     public ArrayList<IArea> getPath(){
         return null;
     }
     // Set the path to be the given argument
     public void setPath(ArrayList<IArea> path){
-
+        this.path = path;
     }
     // Analyze the path according to the type of the analyzer
     public void analyzePath(){
-
+        double runTotal = 0;
+        for (IArea a:
+                path) {
+            runTotal+=a.calcConsumedEnergy();
+        }
+        total = Double.toString(runTotal);
     }
     // Get the result of the analysis according to the type of the analyzer
     public String getAnalysis(){
-        return null;
+        return total;
     }
     public void setAnalysis(String analysis){
-
+        total = analysis;
     }
     // Return the name of the analyzer
     public String toString(){
-        return "Bull";
+        return "Energy Analyzer";
     }
 }
