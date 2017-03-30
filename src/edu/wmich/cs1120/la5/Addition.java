2b0341615a7bb2fa18aab657bc7ed96e51cfd267
Daniel <Daniel@10.80.142.148>
2017-03-29 21:46:37 -0400

    added javadoc

--- src/edu/wmich/cs1120/la5/Addition.java
+++ src/edu/wmich/cs1120/la5/Addition.java
@@ -2,11 +2,15 @@ package edu.wmich.cs1120.la5;
 
 /* Assignment: ...
  * Author: Merrick Rumel
+ * Author: Daniel Toth
  * Date: 3/16/2017
  * Reference: ...
  */
 public class Addition implements IOperation{
     // perform the operation accordingly and return the value
+	/**
+	 * adds two values together to get the sum
+	 */
     public Integer perform(IExpression left, IExpression right){
         return left.getValue() + right.getValue();
     }
